81d0b7a2bed42bc8505cc1564a4ffbba7d79caac
Nitzan Bar <nitzanbar@gmail.com>
2011-08-10 18:07:10 +0300

    Added comments to Login Page

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -28,16 +28,23 @@ namespace MegaStarzWP7
         public Login()
         {
             InitializeComponent();
+            
             _fbClient = new FacebookClient();
+
+            //Browser Loaded Event Handler
             FacebookLoginBrowser.Loaded += new RoutedEventHandler(FacebookLoginBrowser_Loaded);
         }
 
-        // This handles the display a little and also creates the initial URL to navigate to in the browser control
+        /// <summary>
+        /// Perform Login to facebook.
+        /// The function generates login url and navigates the browser to it
+        /// </summary>
         private void LoginToFacebook()
         {
             TitlePanel.Visibility = Visibility.Visible;
             FacebookLoginBrowser.Visibility = Visibility.Visible;
             
+            //Build facebook URL
             var loginParameters = new Dictionary<string, object>
                                       {
                                           { "response_type", "token" }
@@ -46,10 +53,15 @@ namespace MegaStarzWP7
 
             var navigateUrl = FacebookOAuthClient.GetLoginUrl(_appId, null, _extendedPermissions, loginParameters);
 
+            //Navigate to facebook URL
             FacebookLoginBrowser.Navigate(navigateUrl);
         }
 
-        // Browser control is loaded and fully ready for use
+        /// <summary>
+        /// Browser Loaded Event Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void FacebookLoginBrowser_Loaded(object sender, RoutedEventArgs e)
         {
             if (!_loggedIn)
@@ -58,6 +70,13 @@ namespace MegaStarzWP7
             }
         }
 
+        /// <summary>
+        /// Browser Navigated Event Handler
+        /// Check if we have an OAuth Token and parse it
+        /// When We have a good token call loginSucceeded()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FacebookLoginBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
         {
             FacebookOAuthResult oauthResult;
@@ -76,29 +95,34 @@ namespace MegaStarzWP7
             }
         }
 
-        // At this point we have an access token so we can get information from facebook
+        /// <summary>
+        /// Login Success. Get a ticket from the Azure Cloud
+        /// </summary>
         private void loginSucceeded()
         {
             TitlePanel.Visibility = Visibility.Visible;
             FacebookLoginBrowser.Visibility = Visibility.Collapsed;
 
-            var client = new MegaStarzClient(); 
+            var client = new MegaStarzClient("http://localhost:81/Services/WP7/"); 
 
+            //Get a ticket from the clod
             client.GetTicketAsync(new GetTicketRequest()
                                       {
                                           AccessToken = _fbClient.AccessToken
                                       }, 
-                                   (response) =>
+                                   (response) =>        //Callback when we have a ticket
                                        {
                                            if (response != null)
                                            {
+                                               //Save ticket & star object
                                                ((App) Application.Current).starTicket = response.Ticket.ticket;
                                                ((App) Application.Current).star = response.Star;
 
+                                               //Navigate to SongList Page
                                                NavigationService.Navigate(new Uri("/SongListPage.xaml", UriKind.Relative));
                                            
                                            }
-                                           else
+                                           else //Azure could error
                                            {
                                                Dispatcher.BeginInvoke(() => MessageBox.Show("Could not contact Azure server. Please try again later"));
                                            }
